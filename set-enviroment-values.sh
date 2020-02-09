08a14fb74c2d5b45325f559996b816690c29fad8
luispachecoatos <luis.pachecopalencia@atos.net>
2020-02-09 03:19:59 +0000

    Create an Eviroment vars file and included on the main playbook. Vault to Encrypt passwords

---
+++ set-enviroment-values.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+OSP_GUID="ec27"
+TOWER_GUID="3bde"
+OPENTLC_LOGIN="luis.pachecopalencia-atos.net"
+GITHUB_REPO="https://github.com/luispachecoatos/ansible_advance_homework"
+JQ_REPO_BASE="http://www.opentlc.com/download/ansible_bootcamp"
+REGION="us-east-1"
+RH_MAIL_ID="luis.pachecopalencia@atos.net"
